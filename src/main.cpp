cda1824e59f801f0c83d9949a9277fa33460bff8
chetansinghsajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2025-05-10 09:24:21 +0000

    update: fix linker errors

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,28 @@
+#include <exception>
+#include <pistache/endpoint.h>
+#include <pistache/router.h>
+
 import atom_core;
 
-int main() {
-    atom::io::println("hello");
-    return 0;
+using namespace Pistache;
+using namespace Pistache::Http;
+
+auto get_transaction(const Rest::Request& request, Http::ResponseWriter response) -> void
+{
+    response.send(Code::Ok, "Get transaction request recieved.");
+}
+
+auto main() -> int
+{
+    Rest::Router router;
+
+    router.get("/transaction", Rest::Routes::bind(get_transaction));
+
+    auto opts = Http::Endpoint::options().threads(1);
+
+    Address addr(Ipv4::any(), Port(8080));
+    Http::Endpoint server{ addr };
+    server.init(opts);
+    server.setHandler(router.handler());
+    server.serve();
 }
