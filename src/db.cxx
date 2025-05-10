3b735463a7070dbf2831000f10a11c0ed05f06e3
chetansinghsajwan <76040441+chetansinghsajwan@users.noreply.github.com>
2025-05-10 15:00:22 +0000

    update: use env var DB_CONNECT_STRING

--- src/db.cxx
+++ src/db.cxx
@@ -7,11 +7,27 @@ module db;
 import atom_core;
 import std;
 
+using namespace atom;
+
 auto db::init() -> void
 {
     atom::io::println("Connecting to database...");
 
-    pqxx::connection cx{ "" };
+    const auto conn_string_c = std::getenv("DB_CONNECT_STRING");
+    if (conn_string_c == nullptr)
+    {
+        atom::io::println("Cannot connect to database, 'DB_CONNECT_STRING' env var not defined.");
+        return;
+    }
 
-    atom::io::println("Connecting to database done.");
+    const auto conn_string = string_view{ std::getenv("DB_CONNECT_STRING") };
+    if (conn_string.is_empty())
+    {
+        atom::io::println("Cannot connect to database, connection string is empty.");
+        return;
+    }
+
+    pqxx::connection cx{ conn_string.get_data() };
+
+    atom::io::println("Connected to database.");
 }
